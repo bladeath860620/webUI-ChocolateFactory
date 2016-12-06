a9b19cd63ca62f3f4bcc7294329b6fdb52d5c27e
Laxative <never860612@gmail.com>
2016-12-06 16:54:03 +0800

    add bg-img and some code(framework)

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
+  var curpos = 0;
   var containitem = document.getElementsByClassName('container-fluid')[0].children;
   $('#startbutton').click(function() {
+    curpos -= 100;
     for (var i = 0; i < containitem.length; i++) {
-      $(containitem[i]).animate({'top': '-100%'},1000);
+      $(containitem[i]).animate({'top': curpos + '%'},1000);
     }
   });
 });
